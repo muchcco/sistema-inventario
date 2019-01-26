77e1b7a8bc950e10fc814c55baf3657063c0cbcb
kevin muchcco <kevinmuchcco@gmail.com>
2019-01-25 21:51:34 -0500

    modificacion home

--- home.php
+++ home.php
@@ -11,7 +11,7 @@
  <div class="col-md-12">
     <div class="panel">
       <div class="jumbotron text-center">
-         <h1>Esta es su nueva página de inicio</h1>
+         <h1>Esta es LA PAGINA DE INICIO página de inicio</h1>
      
       </div>
     </div>
